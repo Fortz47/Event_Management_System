2eeb5c861c6294bdae6c4fbe8b5e25d65acf8665
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2025-05-23 16:02:18 +0100

    Update: added initialize method to User

--- src/modules/Auth/models/User.ts
+++ src/modules/Auth/models/User.ts
@@ -1,48 +1,51 @@
-import { DataTypes, Model } from 'sequelize';
-import { sequelize } from '../../../configs/database';
+import { DataTypes, Model, Sequelize } from 'sequelize';
 import { DateTime } from 'luxon';
 import { UserAttributes, UserCreationAttributes } from '../../../interfaces/user';
 
 
 class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
-    public id!: number;
-    public name!: string;
-    public email!: string;
-    public password!: string;
-    public readonly createdAt!: DateTime;
-    public readonly updatedAt!: DateTime;
-}
+  declare id: number;
+  declare name: string;
+  declare email: string;
+  declare password_hash: string;
+  declare readonly createdAt: DateTime;
+  declare readonly updatedAt: DateTime;
 
-User.init(
-    {
+  static initialize(sequelize: Sequelize) {
+    User.init(
+      {
         id: {
-            type: DataTypes.INTEGER.UNSIGNED,
-            autoIncrement: true,
-            primaryKey: true,
+          type: DataTypes.INTEGER.UNSIGNED,
+          autoIncrement: true,
+          primaryKey: true,
         },
         name: {
-            type: DataTypes.STRING,
-            allowNull: false,
+          type: DataTypes.STRING,
+          allowNull: false,
         },
         email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true,
-            },
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+          validate: {
+            isEmail: true,
+          },
         },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
+        password_hash: {
+          type: DataTypes.STRING,
+          allowNull: false,
         },
-    },
-    {
+      },
+      {
         sequelize,
         tableName: 'users',
         modelName: 'User',
         timestamps: true,
-    }
-);
+      }
+    )
+  }
+}
+
+
 
 export default User;
