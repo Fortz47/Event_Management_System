7cd52fa20a504c1bed985d9030625aeb6f6caa00
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2025-05-23 16:00:26 +0100

    update: password to password_hash

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -2,12 +2,12 @@ import { Optional } from 'sequelize';
 import { DateTime } from 'luxon';
 
 interface UserAttributes {
-    id: number;
-    name: string;
-    email: string;
-    password: string;
-    createdAt?: DateTime;
-    updatedAt?: DateTime;
+  id?: number;
+  name: string;
+  email: string;
+  password_hash: string;
+  createdAt?: DateTime;
+  updatedAt?: DateTime;
 }
 
 type UserCreationAttributes = Optional<UserAttributes, 'id' | 'createdAt' | 'updatedAt'>;
