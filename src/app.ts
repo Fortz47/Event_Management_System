f66700d6f9fc3ccf13281d1bfca20f0527ca8fd1
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2025-06-04 18:09:00 +0100

    added redis client

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
 import express from "express";
 import { connectDB } from "./configs/db.config";
-import UserRouter from "./modules/UserManagement/routes/UserRoutes";
+import userRouter from "./modules/UserManagement/routes/UserRoutes";
+import authRouter from "./modules/Auth/routes/AuthRoutes";
+import redisClient from "./configs/redis.config";
 
 const app = express();
 app.use(express.json());
 
 // Set up routes
-app.use("/api/users", UserRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
 
-(async () => await connectDB())();
+(async () => {
+  await connectDB();
+  await redisClient.connectRedis();
+})();
 
 export default app;
