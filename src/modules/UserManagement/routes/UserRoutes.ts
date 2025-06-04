cdc1e2fbb456bc77cab75838a5ac6fd5ac84e104
akaolisa atuokwu <akaolisa.atuokwu@snapnetsolutions.com>
2025-06-04 11:54:06 +0100

    add UserRoutes for user management endpoints

---
+++ src/modules/UserManagement/routes/UserRoutes.ts
@@ -0,0 +1,38 @@
+import { Router } from "express";
+import UserController from "../controllers/UserController";
+import { validateRequest } from "../../../middleware/validate-user.middleware";
+import {
+  createUserSchema,
+  updateUserSchema,
+} from "../../../schemas/users.schema";
+
+class UserRoutes extends UserController {
+  private router: Router;
+
+  constructor() {
+    super();
+    this.router = Router();
+    this.initializeRoutes();
+  }
+
+  public getRouter() {
+    return this.router;
+  }
+
+  private initializeRoutes() {
+    this.router.get("/", this.getAllUsers);
+    this.router.route("/:id").get(this.getUserById);
+    this.router.get("/:id", this.getUserById);
+    this.router.post("/", validateRequest(createUserSchema), this.createUser);
+    this.router.put("/:id", validateRequest(createUserSchema), this.updateUser);
+    this.router.patch(
+      "/:id",
+      validateRequest(updateUserSchema),
+      this.updateUser
+    );
+    this.router.delete("/:id", this.deleteUser);
+  }
+}
+
+const UserRouter = new UserRoutes().getRouter();
+export default UserRouter;
