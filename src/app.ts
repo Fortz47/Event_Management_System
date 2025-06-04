0188559a59ce16df5361a6c384c0a079e37193ee
akaolisa atuokwu <akaolisa.atuokwu@snapnetsolutions.com>
2025-06-04 11:46:48 +0100

    refactor

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import { connectDB } from "./configs/database";
-
+import { connectDB } from "./configs/db.config";
+import UserRouter from "./modules/UserManagement/routes/UserRoutes";
 
 const app = express();
 app.use(express.json());
+
+// Set up routes
+app.use("/api/users", UserRouter);
 
 (async () => await connectDB())();
 
